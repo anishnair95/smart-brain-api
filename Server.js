f5e4b28126e01ff64a30ce0e9f570938c37e1898
Anish Nair <anishnair95@gmail.com>
2022-04-12 11:20:12 +0530

    Server fix| Server.js

--- Server.js
+++ Server.js
@@ -20,7 +20,9 @@ const db = knex({
     client: 'pg',
     connection: {
         connectionString:process.env.DATABASE_URL,
-        ssl:false
+        ssl: {
+            rejectUnauthorized: false
+          }
 
     }
 });
